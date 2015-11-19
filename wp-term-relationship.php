98f6c3ca1a47a46d653be001f733b5a9344b0d94
KamataRyo <mugil.cephalus+github@gmail.com>
2015-11-20 01:50:54 +0900

    add main module

---
+++ wp-term-relationship.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: wp term relationships
+Plugin URI: http://kamataryo.github.io/wp-term-relationships/
+Description: wordpress plugin to define relationships between terms. synonym, antnym and so on.
+Author: Kamata Ryo
+Version: 1.0.0
+Author URI: http://kamataryo.github.io/wp-term-relationships/
+Domain Path: /languages
+Text Domain: wp-term-relationships
+*/
